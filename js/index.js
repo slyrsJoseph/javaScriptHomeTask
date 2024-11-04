d72b34a383195b1821f06a8b9502b6ba53b17419
slyrsJoseph <boxer_slayers@yahoo.com>
2024-11-04 22:52:18 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,85 @@
+//////////TASK 1
+// let ask = prompt("What is your name? ");
+// alert("Hello " + ask);
+
+/////////TASK 2
+
+// let age = prompt("Enter your year of birth");
+// alert("User is " + (2024 - age) + " years old");
+
+////////TASK 3
+// let side_square = prompt("enter the length of the side of the square ");
+// alert("Perimeter of square = " + side_square * 4);
+
+///////TASK 4
+// let circle_radius = prompt("Enter the radius of the circle");
+// alert("Are of the circle = " + 3.14 * circle_radius ** 2);
+
+////////TASK 5
+// let distance = prompt("Enter the distance between two points");
+// let time = prompt("Enter the time you want get to destination");
+
+// alert("The speed you need to get there in time " + distance / time + " km/h");
+
+///////TASK 6
+
+// const USD_TO_MANAT = 1.7;
+
+// let dollar = prompt("Enter the sum in dollars");
+
+// alert("Sum in manat = " + dollar * USD_TO_MANAT);
+
+///////TASK 7
+// let flash_capacity = prompt("Enter the flash drive capacity in gb");
+
+// alert(
+//   "The number of files of 820 MB that fit on a flash drive" +
+//     (flash_capacity * 1000) / 820
+// );
+
+//////TASK 8
+
+// let money_in_pocket = parseFloat(
+//   prompt("Enter the count of money in your pocket")
+// );
+// let chocolate_price = parseFloat(prompt("Enter the price of the chocolate"));
+
+// let chocolate_count = money_in_pocket / chocolate_price;
+// let money_remains = money_in_pocket - chocolate_count * chocolate_price;
+
+// alert(
+//   "The number of chocolate you can buy " +
+//     chocolate_count +
+//     "The rest of money " +
+//     money_remains
+// );
+
+////////TASK 9
+// while (true) {
+//   let number = prompt("Enter the number: ");
+//   number = parseInt(number);
+
+//   if (number >= 100 && number <= 999) {
+//     let hundreds = number / 100;
+
+//     let tens = (number % 100) / 10;
+//     let units = number % 10;
+
+//     let reverse = units * 100 + tens * 10 + hundreds;
+
+//     alert("Reversed number " + reverse);
+//     break;
+//   } else {
+//     alert("Enter the right number");
+//   }
+// }
+
+//////////TASK 10
+
+// let number = prompt("Enter the even number: ");
+// if (number % 2 === 0) {
+//   alert("Even number");
+// }
+// if (number % 2 !== 0) {
+//   alert("Odd number");
+// }
